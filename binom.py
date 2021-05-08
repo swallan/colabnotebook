82f072f1e670eaea0cda78de86b92860f03a597c
Sam Wallan <44255917+swallan@users.noreply.github.com>
2021-05-08 16:27:48 -0700

    mmm

--- binom.py
+++ binom.py
@@ -10,6 +10,7 @@ from mpmath import gamma, pi, erf, exp, sqrt, quad, inf, mpf
 from mpmath import npdf as phi
 from mpmath import ncdf as Phi
 from mpmath import mp
+import numpy as np
 
 
 def _cdf_mp(q, k, nu, method, verbose=True, error=True, dps=15):
@@ -32,39 +33,39 @@ def cdf_mp_gl(q, k, nu, verbose=True, error=True, dps=15):
     return _cdf_mp(q, k, nu, method='gauss-legendre', verbose=verbose,
                   error=error, dps=dps)
 
-import srd
-import ctypes
-from scipy._lib._ccallback import LowLevelCallable
-import numpy as np
-import scipy.integrate as integrate
+# import srd
+# import ctypes
+# from scipy._lib._ccallback import LowLevelCallable
+# import numpy as np
+# import scipy.integrate as integrate
 
 
-def cython_cdf(q, k, v):
-    # atol = 10**-(dps)
-    usr_data = np.array([q, k, v], float).ctypes.data_as(ctypes.c_void_p)
-    llc = LowLevelCallable.from_cython(srd,
-                                    "genstudentized_range_cdf",
-                                    usr_data)
-    ranges = [(-np.inf, np.inf), (0, np.inf)]
-    opts = dict(epsabs=10e-14)
-    res = integrate.nquad(llc, ranges=ranges, opts=opts)[0]
-    return res
+# def cython_cdf(q, k, v):
+#     # atol = 10**-(dps)
+#     usr_data = np.array([q, k, v], float).ctypes.data_as(ctypes.c_void_p)
+#     llc = LowLevelCallable.from_cython(srd,
+#                                     "genstudentized_range_cdf",
+#                                     usr_data)
+#     ranges = [(-np.inf, np.inf), (0, np.inf)]
+#     opts = dict(epsabs=10e-14)
+#     res = integrate.nquad(llc, ranges=ranges, opts=opts)[0]
+#     return res
     
-from statsmodels.stats.libqsturng import psturng
-def statsmodels_cdf(q, k, v):
-  # `psturng` return the `alpha` value, which we 
-  # convert to a `pvalue`. 
-  return 1 - psturng(q, k, v)
+# from statsmodels.stats.libqsturng import psturng
+# def statsmodels_cdf(q, k, v):
+#   # `psturng` return the `alpha` value, which we 
+#   # convert to a `pvalue`. 
+#   return 1 - psturng(q, k, v)
 
 
 # number of combinations:
 n = int(100 ** (1/3))
 
-qs = (np.random.rand(n) ** 5 + .001) * 1000
+qs = (np.random.rand(n * 2) ** 5 + .001) * 150
 qs = np.unique(qs)
-ks = ((np.random.rand(n) ** 5 )* 100000).astype(int) % 100 + 2
+ks = ((np.random.rand(n) ** 5 )* 100000).astype(int) % 50 + 2
 vs = np.unique(ks)
-vs = ((np.random.rand(n) ** 5) * 100000).astype(int) % 100 + 2
+vs = ((np.random.rand(n) ** 5) * 100000).astype(int) % 50 + 2
 vs = np.unique(vs)
 
 from itertools import combinations, product
@@ -80,12 +81,17 @@ results_statsmodels = dict()
     
 for q, k, v in combinations:
     print(f"{q} - {k} - {v}")
-    results_cython[f"{q}-{k}-{v}"] = cython_cdf(q, k, v)
-    # results_mpmath[f"{q}-{k}-{v}"] = cdf_mp_gl(q, float(k), float(v))
+    # results_cython[f"{q}-{k}-{v}"] = cython_cdf(q, k, v)
+    results_mpmath[f"{q}-{k}-{v}"] = cdf_mp_gl(q, float(k), float(v))
     # results_statsmodels[f"{q}-{k}-{v}"] = statsmodels_cdf(q, k, v)
 
-with open("mapmathres.txt", "w+") as f:
-    f.write(json.dumps(results_cython))    
+# results = dict()
+# results["results_cython"] = results_cython
+# results["results_mpmath"] = results_mpmath
+# results["results_statsmodels"] = results_statsmodels
+
+with open("results.txt", "w+") as f:
+    f.write(json.dumps(results_mpmath))    
     
     
 
